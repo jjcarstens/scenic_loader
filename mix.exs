7605f4421067a4b093e32a1b739ec17ed85c7235
Jon Carstens <jjcarstens@me.com>
2020-02-27 13:46:37 -0600

    prep for publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule ScenicLoader.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/jjcarstens/scenic_loader"
+  @version "0.1.0"
+
   def project do
     [
       app: :scenic_loader,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "When you just need to show a loading spinner in scenic",
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -15,6 +21,22 @@ defmodule ScenicLoader.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    }
   end
 
   defp deps do
